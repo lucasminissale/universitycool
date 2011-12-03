b993e68bd69a0a63a474bcb8ab6c3286099b20b6
Lucas <lucasminissale@gmail.com>
2011-12-03 14:37:59 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Universitycool::Application.config.secret_token = '7ec640dd53f393f99461c9b71429c2b5002f8dd6081283589730e1d41e10c81929ffbefc0e7ad14954dded87bf97d798d4360015f73ef7a2db2d9bd2ef62e64d'
